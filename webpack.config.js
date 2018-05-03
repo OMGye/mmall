1a03c1be06551f835c93f752b5301cb1f3bb0fe9
OMGye <1252078414@qq.com>
2018-05-03 21:17:59 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -100,7 +100,7 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
         new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
-        new HtmlWebpackPlugin(getHtmlConfig('user-password-update', '修改密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-password-reset', '修改密码')),
         new HtmlWebpackPlugin(getHtmlConfig('user-find-password', '找回密码')),
         new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
         new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认页')),
